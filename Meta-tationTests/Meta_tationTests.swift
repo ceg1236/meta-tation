c4722d9b69db7c4497d86de96c27162ad4b87271
Carl Goldberg <carlgoldberg1236@gmail.com>
2015-02-17 14:17:57 -0800

    Initial Commit

---
+++ Meta-tationTests/Meta_tationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meta_tationTests.swift
+//  Meta-tationTests
+//
+//  Created by Carl Goldberg on 2/17/15.
+//  Copyright (c) 2015 Tangent Circles. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Meta_tationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
