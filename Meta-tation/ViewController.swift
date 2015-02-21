de0ee677d0abd54c0f2d1a386497e8ea4971a5e3
Carl Goldberg <carlgoldberg1236@gmail.com>
2015-02-21 12:20:36 -0800

    Mapview on SF, Start button with gradient, no functionality

--- Meta-tation/ViewController.swift
+++ Meta-tation/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var tsongaMap: MKMapView!
+    @IBAction func startSession(sender: AnyObject) {
+        
+    }
+    
+    var lat:CLLocationDegrees = 37.7836449
+    var lng:CLLocationDegrees = -122.4091387
+    
+    var latDelta:CLLocationDegrees = 0.1
+    var lngDelta:CLLocationDegrees = 0.1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Snippet for extracting JSON with SwiftyJSON
+        let url = NSURL(string: "http://localhost:8003/meditators")
+        var request = NSURLRequest(URL: url!)
+        var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
+        if data != nil {
+            var hoge = JSON(data: data!)
+            println(hoge)
+            
+            var latlng = hoge[0]["latlng"]
+            println(latlng)
+            
+            if latlng != nil {
+                lat = latlng[0].double!
+                lng = latlng[1].double!
+                
+                latDelta = 0.01
+                lngDelta = 0.01
+
+            }
+            
+            var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lngDelta)
+            
+            var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, lng)
+            
+            var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            
+            tsongaMap.setRegion(region, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
